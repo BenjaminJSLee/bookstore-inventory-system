de9d81dc45230bc7ac7d12c1f8f2d31e19b63001
BenjaminJSLee <benjaminjslee@gmail.com>
2021-02-12 13:54:38 -0800

    Minor framework changes

--- server/server.js
+++ server/server.js
@@ -12,21 +12,12 @@ const cors       = require("cors");
 const knex = require('knex')({
   client: 'pg',
   connection: {
-    host : process.env.DB_HOST,
-    user : process.env.DB_USER,
-    password : process.env.DB_PASSWORD,
-    database : process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   }
 });
-
-// PG database client/connection setup
-// const { Client } = require("pg");
-// const db = new Client({
-//   connectionString: process.env.DATABASE_URL});
-
-// db
-//   .connect()
-//   .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
 
 app.use(cors());
 app.use(cookieSession({
@@ -35,6 +26,8 @@ app.use(cookieSession({
 }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// app.use(authenticate);
 
 // import routes and middleware below
 
