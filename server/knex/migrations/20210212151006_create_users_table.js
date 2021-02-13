52bc831c9faba4ccc277f9afc36b5650a1f4e2d0
BenjaminJSLee <benjaminjslee@gmail.com>
2021-02-12 17:50:09 -0800

    Finalize first version of seeds and migrations

--- server/knex/migrations/20210212151006_create_users_table.js
+++ server/knex/migrations/20210212151006_create_users_table.js
@@ -2,7 +2,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable('users', function(table) {
     table.increments();
-    table.string('email').notNullable();
+    table.string('username').notNullable();
     table.string('password').notNullable();
   });
 };
