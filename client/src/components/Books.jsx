a02be3dbe6530508e41fc5d20dbb77b427602dc5
BenjaminJSLee <benjaminjslee@gmail.com>
2021-02-16 11:11:48 -0800

    Move formatting into functions

--- client/src/components/Books.jsx
+++ client/src/components/Books.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import './Books.css';
 
+const formatCents = (price) => {
+  const priceArr = price.toString().split('');
+  while (priceArr.length < 3) priceArr.unshift("0");
+  priceArr.splice(-2,0,'.');
+  return priceArr.join('');
+};
+
+const formatDate = (date) => {
+  const tmp = new Date(date);
+  return tmp.toDateString().slice(4);
+};
+
 const Books = (props) => {
 
   const books = props.books.map((book) => {
-    const priceArr = book.price.toString().split('');
-    while (priceArr.length < 3) priceArr.unshift("0");
-    priceArr.splice(-2,0,'.');
-    const price = priceArr.join('');
+    const price = formatCents(book.price);
+    const date = formatDate(book.date_published);
     console.log(price);
     return (
       <tr key={book.id}>
         <td>{book.title}</td>
         <td>{book.status}</td>
         <td>{book.author}</td>
-        <td>{(new Date(book.date_published)).toDateString()}</td>
+        <td>{date}</td>
         <td>{book.publisher}</td>
         <td>${price}</td>
       </tr>
