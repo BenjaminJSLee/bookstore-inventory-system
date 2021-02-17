2a706ce2bd6438befc88f59e6981ba746df5f19b
BenjaminJSLee <benjaminjslee@gmail.com>
2021-02-16 18:53:09 -0800

    Add 'on delete cascade' to bookstore_books migration

--- server/knex/migrations/20210212151044_create_bookstore_books_table.js
+++ server/knex/migrations/20210212151044_create_bookstore_books_table.js
@@ -2,8 +2,8 @@
 exports.up = function(knex) {
   return knex.schema.createTable('bookstore_books', function(table) {
     table.increments();
-    table.integer('bookstore_id').references('id').inTable('bookstores');
-    table.integer('book_id').references('id').inTable('books');
+    table.integer('bookstore_id').references('id').inTable('bookstores').onDelete("CASCADE");
+    table.integer('book_id').references('id').inTable('books').onDelete("CASCADE");
     table.unique(['bookstore_id','book_id']);
     table.integer('stock').notNullable();
   });
